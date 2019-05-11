e4c6698ee34b264f56c7809aec2671c39d04e337
mike <mpakleung@gmail.com>
2019-05-11 15:58:27 -0400

    Add metabox for Start Date custom field

--- events-plugin.php
+++ events-plugin.php
@@ -19,11 +19,26 @@ if (!function_exists('add_action')) {
 }
 
 
+if (class_exists('EventsPlugin')) {
+	$eventsPlugin = new EventsPlugin();
+	$eventsPlugin->registerScripts();
+}
+
+// Activation
+register_activation_hook(__FILE__, array($eventsPlugin, 'activate'));
+
+
+// Deletion
+register_deactivation_hook(__FILE__, array($eventsPlugin,'deactivate'));
+
+
+
+
 
 class EventsPlugin {
 
 	function __construct() {
-		add_action('init', array($this, 'custom_post_type'));
+		add_action('init', array($this, 'event_custom_post_type'));
 
 	}
 
@@ -43,24 +58,74 @@ class EventsPlugin {
 
 	}
 
-	function custom_post_type() {
-		register_post_type('event', ['public' => true, 'label' => 'Events']);
+
+
+
+	function event_custom_post_type() {
+		//register_post_type('event', ['public' => true, 'label' => 'Events']);
+		
+		register_post_type("event", array(
+			'labels' => array(
+				'name' => 'All Events',
+				'singular_name' => 'Event',
+			),
+			'description' => 'Events which will be displayed',
+			'public' => true,
+			//'supports' => array('title', 'editor', 'custom-fields'),
+			'register_meta_box_cb' => 'event_date_meta_box'
+
+		));
+
+		register_taxonomy_for_object_type('category','event');
+
+		add_action( 'add_meta_boxes', 'event_date_meta_box' );
+		add_action('save_post', 'event_date_save_postdata');
+
+
 	}
+
 
 	function enqueue() {
 		wp_enqueue_style("mypluginstyle", plugins_url('/assets/style.css', __FILE__));
 		wp_enqueue_script("mypluginscript", plugins_url('/assets/script.js', __FILE__));
+		
 	}
 }
 
-if (class_exists('EventsPlugin')) {
-	$eventsPlugin = new EventsPlugin();
-	$eventsPlugin->registerScripts();
+// https://developer.wordpress.org/plugins/metadata/custom-meta-boxes/
+function event_date_meta_box() {
+
+    add_meta_box(
+        'date_id',
+        'Dates',
+        'event_date_html_callback',
+        'event'
+    );
 }
 
-// Activation
-register_activation_hook(__FILE__, array($eventsPlugin, 'activate'));
+
+function event_date_html_callback( $post ) {
+	$startDate = get_post_meta($post->ID, '_event_start_meta_key');
+
+?>
+ <label for="event-start">Start Date</label>
+ <input type="date" id="event-start" name="event-start" value="<?php echo $startDate[0];  ?>">
+ <!--<label for="wporg_field">End Date</label>
+ <input type="date" id="end-date" name="date-end" value="2019-05-12">-->
+<?php
+}
 
 
-// Deletion
-register_deactivation_hook(__FILE__, array($eventsPlugin,'deactivate'));
+function event_date_save_postdata($post_id) {
+    if (array_key_exists('event-start', $_POST)) {
+    	if ($_POST['event-start'] != "") {
+	        update_post_meta(
+	            $post_id,
+	            '_event_start_meta_key',
+	            $_POST['event-start']
+	        );
+    	}
+
+    }
+}
+
